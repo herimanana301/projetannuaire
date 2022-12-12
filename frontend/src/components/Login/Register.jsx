3f103eccd9075bd540749744f8c2be7fa32205b3
Louen52 <louen.maginot@gmail.com>
2022-12-12 15:13:26 +0100

    adding style

--- frontend/src/components/Login/Register.jsx
+++ frontend/src/components/Login/Register.jsx
@@ -8,15 +8,17 @@ function Register() {
         src="https://cdn.discordapp.com/attachments/1046791933583949824/1050694287517569064/logo.png"
         alt="logo"
       />
-      <label htmlFor="email">Email</label>
-      <input id="email" name="email" type="email" />
-      <label htmlFor="text">First name</label>
-      <input id="text" name="firstname" type="text" />
-      <label htmlFor="text">Last name</label>
-      <input id="text" name="lastname" type="text" />
-      <label htmlFor="password">Password</label>
-      <input id="password" name="password" type="password" />
-      <button type="submit">Register</button>
+      <div className="form">
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" />
+        <label htmlFor="text">First name</label>
+        <input id="text" name="firstname" type="text" />
+        <label htmlFor="text">Last name</label>
+        <input id="text" name="lastname" type="text" />
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+        <button type="submit">Register</button>
+      </div>
     </div>
   );
 }
