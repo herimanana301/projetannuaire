5839a5aeaea842aecdce323adec40117b1f6dcfc
Herimanana <herimanana301@gmail.com>
2023-01-09 14:01:32 +0100

    redirection after login and error message setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cookies from "js-cookie";
 import Register from "./components/Login/Register";
 import Login from "./components/Login/Login";
 import FirstPage from "./components/Login/FirstPage";
 import ContactPage from "./components/PhoneBook/ContactPage";
 import CreateContact from "./components/PhoneBook/CreateContact";
-import ForLogedPage from "./components/ForLogedPage";
+import { ForLogedPage, ToHome } from "./components/ForLogedPage";
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorLog, setErrorLog] = useState();
 
-  const login = () => {
-    Axios.post("http://localhost:3305/login", {
-      email,
-      password,
-    })
-      .then((response) => {
-        Cookies.set("access_token", response.data.accessToken, {
-          sameSite: "Lax",
-        });
-        console.warn(response.data.accessToken);
-      })
-      .catch((error) => {
-        if (error) {
-          setErrorLog(true);
-        }
-      });
-  };
   return (
     <Router>
       <div>
@@ -38,14 +19,14 @@ function App() {
           <Route path="/" element={<FirstPage />} />
           <Route
             path="/login"
-            element={
+            element={ToHome(
               <Login
                 setEmail={setEmail}
+                email={email}
+                password={password}
                 setPassword={setPassword}
-                login={login}
-                errorLog={errorLog}
               />
-            }
+            )}
           />
           <Route path="/register" element={<Register />} />
           <Route path="/home" element={ForLogedPage(<ContactPage />)} />
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import "./login.css";
 import PropTypes from "prop-types";
+import Axios from "axios";
+import Cookies from "js-cookie";
+
 import logo from "../../assets/logo.png";
 
-function Login({ setEmail, setPassword, login }) {
+function Login({ setEmail, setPassword, email, password }) {
+  const [errorState, setErrorState] = useState();
+  const handleLogin = () => {
+    Axios.post("http://localhost:3305/login", {
+      email,
+      password,
+    })
+      .then((response) => {
+        Cookies.set("access_token", response.data.accessToken, {
+          sameSite: "Lax",
+        });
+        window.location.reload(false);
+      })
+      .catch((error) => {
+        if (error) {
+          setErrorState(true);
+        }
+      });
+  };
   return (
     <div className="firstpage">
+      {errorState ? <h1>Vos identifiants sont incorrect</h1> : null}
       <img src={logo} alt="logo" />
       <div className="wording">
         <label htmlFor="email">Email</label>
@@ -26,7 +48,7 @@ function Login({ setEmail, setPassword, login }) {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <button className="login-button" type="submit" onClick={login}>
+        <button className="login-button" type="submit" onClick={handleLogin}>
           Login
         </button>
       </div>
@@ -37,7 +59,8 @@ function Login({ setEmail, setPassword, login }) {
 Login.propTypes = {
   setEmail: PropTypes.func.isRequired,
   setPassword: PropTypes.func.isRequired,
-  login: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 };
 
 export default Login;
